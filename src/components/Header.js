7ad7c7c199754cf5e612de83f9d6556889d878d6
bozgen <bilalozgen00@gmail.com>
2022-02-17 04:52:15 +0300

    refactor Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,23 @@ export default function Header(props){
     return(
         <header>
             <section className="header-times">
-                { props.gameScreen==="game" && <h3>time: <span className="game-time">{props.gameTime.newTime}s</span></h3>}
-                { props.gameTime.best && <h3>best: <span className="bestTime">{props.gameTime.best}s</span></h3>}
+                {
+                props.gameScreen==="game" &&
+                <h3>
+                    time: <span className="game-time">
+                        {props.gameTime.newTime}s
+                    </span>
+                </h3>
+                }
+                {
+                props.gameTime.best &&
+                <h3>
+                    best: <span className="bestTime">
+                        {props.gameTime.best}s
+                    </span>
+                </h3>
+                }
+
             </section>
             <h1>Minesweeper</h1>
         </header>
